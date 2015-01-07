d6fa00bb1cc28923c6729da2c0ea3c71977532a9
Daniel St. Jules <danielst.jules@gmail.com>
2015-01-07 09:09:07 -0800

    Conditionally load Stringy\create

--- src/Create.php
+++ src/Create.php
@@ -2,16 +2,18 @@
 
 namespace Stringy;
 
-/**
- * Creates a Stringy object and returns it on success.
- *
- * @param  mixed   $str      Value to modify, after being cast to string
- * @param  string  $encoding The character encoding
- * @return Stringy A Stringy object
- * @throws \InvalidArgumentException if an array or object without a
- *         __toString method is passed as the first argument
- */
-function create($str, $encoding = null)
-{
-    return new Stringy($str, $encoding);
+if (!function_exists('Stringy\create')) {
+    /**
+     * Creates a Stringy object and returns it on success.
+     *
+     * @param  mixed   $str      Value to modify, after being cast to string
+     * @param  string  $encoding The character encoding
+     * @return Stringy A Stringy object
+     * @throws \InvalidArgumentException if an array or object without a
+     *         __toString method is passed as the first argument
+     */
+    function create($str, $encoding = null)
+    {
+        return new Stringy($str, $encoding);
+    }
 }
