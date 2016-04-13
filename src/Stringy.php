6ded25597fea4e212ab9daf38bcfad73f56ea333
Munawwar <munawwarfiroz@hotmail.com>
2016-04-13 23:09:01 +0400

    Added matches() method.

--- src/Stringy.php
+++ src/Stringy.php
@@ -1786,6 +1786,19 @@ class Stringy implements Countable, IteratorAggregate, ArrayAccess
 
         return $stringy;
     }
+    
+    /**
+     * Does a preg_match using pattern returns matches.
+     *
+     * @param  string $pattern Regex pattern to match against
+     * @return array  preg_match matches.
+     */
+    public function matches($pattern)
+    {
+        $matches = [];
+        preg_match($pattern, $this->str, $matches);
+        return $matches;
+    }
 
     /**
      * Returns true if $str matches the supplied pattern, false otherwise.
